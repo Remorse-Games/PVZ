9d6c58defeb493924659b3c55e72632e67a304fc
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2020-10-17 17:01:07 +0700

    +Add: enemy spawner

---
+++ Assets/Script/Enemy/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+    public float spawnDelay = 1f; //spawnDelay=1/x secs
+    private void Start()
+    {
+        StartSpawn(10);
+    }
+    private void StartSpawn(int spawnAmount)
+    {
+        StartCoroutine(Spawning(spawnAmount));
+    }
+    private IEnumerator Spawning(int spawnAmount)
+    {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            Rigidbody2D rb = Instantiate(enemyPrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.right;
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
