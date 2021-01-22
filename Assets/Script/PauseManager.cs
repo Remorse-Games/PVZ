e40e21249906547c151c13b8a01877df6b64f59b
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2021-01-22 12:21:49 +0700

    Pause

---
+++ Assets/Script/PauseManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public void Pause()
+    {
+        Time.timeScale = (Time.timeScale == 0f) ? 1f : 0f;
+    }
+}
